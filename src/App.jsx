ba80bacc207f1cbcf4e514e1245980a5de8784a3
OleksandrOleksyuk <oleksandr.oleksyuk97@icloud.com>
2023-03-12 23:17:38 +0100

    v 0.01 film explorer

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import SearchIcon from "./assets/search.svg";
+import MovieCard from "./MovieCard";
+import { API_URL } from "./secretKey";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      setMovies(data.Search);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies("The Dark Knight");
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center p-10">
+      <h1 className="mb-5 text-5xl font-bold text-orange-700">FilmExplorer</h1>
+      <div className="my-3 flex w-full items-center justify-center rounded-full p-1 shadow-lg shadow-orange-700 md:w-8/12 md:p-6">
+        <input
+          className="active:bg-red/500 flex-1 border-none pl-4 text-2xl font-medium text-orange-900"
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <div className="animate rounded-full bg-orange-500 p-4">
+          <img
+            className="h-6 w-6 cursor-pointer sm:h-10 sm:w-10"
+            src={SearchIcon}
+            alt="searchIcon"
+            onClick={() => searchMovies(searchTerm)}
+          />
+        </div>
+      </div>
+      {movies?.length > 0 ? (
+        <div className="mt-12 flex w-full flex-wrap items-center justify-center">
+          {movies.map((movie, i) => (
+            <MovieCard key={i} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="mt-12 flex w-full items-center justify-center">
+          <h2 className="text-2xl font-medium text-red-600">No Movie found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="relative m-6 h-[450px] w-80 overflow-hidden rounded-xl border-none shadow-lg shadow-orange-700">
+      <div className="">
+        <img
+          className="h-full w-full object-cover"
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={movie.Title}
+        />
+      </div>
+      <div className="absolute top-0 left-0 z-10 flex h-full w-full flex-col justify-end bg-gradient-to-b from-transparent to-slate-900  p-5">
+        <div className="flex flex-col justify-between">
+          <h3 className="text-xl font-semibold text-orange-500">
+            {movie.Title}
+          </h3>
+          <div className="flex items-center justify-between">
+            <p className="font-bold uppercase text-orange-300">{movie.Type}</p>
+            <p className="font-semibold text-orange-300">{movie.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
